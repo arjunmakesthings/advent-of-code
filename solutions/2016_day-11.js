1cd83cbfe8b6bc15a52a6511b025cfcc5d5d6755
arjun <82059571+arjunmakesthings@users.noreply.github.com>
2026-06-22 14:47:29 -0400

    work.

---
+++ solutions/2016_day-11.js
@@ -0,0 +1,19 @@
+/*
+
+terms: 
+mc -> microchip; g -> generator; cg = compatible generator; cmc -> compatible microchip. 
+
+possible combinations:
+mc + mc = ok
+cg + cmc = ok
+g + g = ok
+cg + cmc + g || cg + cmc + mc = ok
+
+restrictions: 
+entry = floor 1; assembler = floor 4.
+
+lift >= 1 item && lift <= 2 items.
+
+*/
+
+
