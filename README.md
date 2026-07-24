7741f70ccc7af38cb8133baf9d230c9519155873
arjun <82059571+arjunmakesthings@users.noreply.github.com>
2026-07-24 13:47:04 -0400

    add readme.'

---
+++ README.md
@@ -0,0 +1,7 @@
+solutions for [advent-of-code](https://adventofcode.com) puzzles. 
+
+[discovered](https://arjunmakesthings.github.io/itp-blog/recurse-center/advent-of-code) during my time at the [recurse-center](https://www.recurse.com), 2026.
+
+---
+
+each person gets a unique input, and the creator of the puzzle asks us to hide our inputs. so, my inputs are not tracked on this repository.
---
+++ solutions/2023/day_24_2.js
@@ -0,0 +1,120 @@
+/*
+https://adventofcode.com/2023/day/24
+
+ask: check for intersections in a 2-d grid, for hailstones moving at constant velocities. 
+
+look for interesections when x || y >= 200000000000000 && x || y <= 400000000000000. 
+
+solution: 
+realized you can't really step through the simulation, because they may collide when t != int. 
+*/
+
+const fs = require("node:fs");
+const input = fs.readFileSync("../../inputs/2023/day-24.txt", "utf-8");
+
+let test_input = [
+  {
+    x: 0,
+    y: 0,
+    z: 0,
+    vx: 1,
+    vy: 1,
+    vz: 0,
+  },
+  {
+    x: 2,
+    y: 0,
+    z: 0,
+    vx: -1,
+    vy: 1,
+    vz: 0,
+  },
+];
+
+function parse(input) {
+  return input
+    .trim()
+    .split("\n")
+    .filter(Boolean)
+    .map((line) => {
+      const [pos, vel] = line.split(" @ ");
+      const [x, y, z] = pos.split(", ").map(Number);
+      const [vx, vy, vz] = vel.split(", ").map(Number);
+
+      return { x, y, z, vx, vy, vz };
+    });
+}
+
+function solve(h) {
+  //   const test_area = { min: 200000000000000, max: 400000000000000 };
+  const test_area = { min: 200000000000000, max: 400000000000000 };
+  let intersections = 0;
+  let hailstones = h;
+
+  let hailstones_to_check = hailstones.filter(
+    (h) =>
+      h.x >= test_area.min &&
+      h.x <= test_area.max &&
+      h.y >= test_area.min &&
+      h.y <= test_area.max,
+  );
+
+  //preliminary check to avoid starting point intersections.
+  for (let i = 0; i < hailstones_to_check.length; i++) {
+    for (let j = 0; j < hailstones_to_check.length; j++) {
+      if (i == j) {
+        continue;
+      } else {
+        intersections += check(hailstones_to_check[i], hailstones_to_check[j])
+          ? 1
+          : 0;
+      }
+    }
+  }
+
+  //till you have valid hailstones to check:
+  while (hailstones_to_check.length >= 2) {
+    //compute new positions:
+    for (let i = 0; i < hailstones_to_check.length; i++) {
+      hailstones_to_check[i].x += hailstones_to_check[i].vx;
+      hailstones_to_check[i].y += hailstones_to_check[i].vy;
+    }
+
+    //remake array to check:
+    hailstones_to_check = hailstones_to_check.filter(
+      (h) =>
+        h.x >= test_area.min &&
+        h.x <= test_area.max &&
+        h.y >= test_area.min &&
+        h.y <= test_area.max,
+    );
+
+    //check & count intersections:
+    for (let i = 0; i < hailstones_to_check.length; i++) {
+      for (let j = i + 1; j < hailstones_to_check.length; j++) {
+        if (i === j) {
+          continue;
+        } else {
+          intersections += check(hailstones_to_check[i], hailstones_to_check[j])
+            ? 1
+            : 0;
+        }
+      }
+    }
+  }
+
+  return intersections;
+}
+
+//helper;
+function check(obj1, obj2) {
+  //accepts objects with x,y,z && vx, vy, vz.
+
+  if (obj1.x == obj2.x && obj1.y == obj2.y) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(solve(parse(input)));
