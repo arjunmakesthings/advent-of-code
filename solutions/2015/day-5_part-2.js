f353285528a3b0820fc3a4ffed8a3dcffe6f36da
arjun <82059571+arjunmakesthings@users.noreply.github.com>
2026-07-24 13:43:16 -0400

    rename.

---
+++ solutions/2015/day-5_part-2.js
@@ -0,0 +1,14 @@
+//day 5; part 2.
+
+const fs = require("node:fs");
+const input = fs.readFileSync("../../inputs/2015/day-5.txt", "utf-8");
+
+function solve_part_2(txts) {
+	const strs = txts.split("\n");
+	let nice_strings = 0;
+
+	return nice_strings;
+}
+
+//helpers:
+console.log(solve_part_2(input));
